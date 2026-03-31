06c592f53e862d6afcc180827da3a75b92aaf303
Tuany-rvlh <persilvatuany@gmail.com>
2026-03-31 14:08:35 -0300

    Aula06Ex01

--- Aula04/Ex03_jogoRPG/obj/Debug/net9.0/Ex03_jogoRPG.AssemblyInfo.cs
+++ Aula04/Ex03_jogoRPG/obj/Debug/net9.0/Ex03_jogoRPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex03_jogoRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+407bff6c693591fc2b4ebee6f58a67e9b9b76e3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+422791c0289e280f4a32bb2928b705726c2fe99b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex03_jogoRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex03_jogoRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
