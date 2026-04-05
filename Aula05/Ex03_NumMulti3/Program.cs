1812df8139bd76c3ced9dc7a3dc7b50b52e4118d
Tuany-rvlh <persilvatuany@gmail.com>
2026-04-05 18:43:14 -0300

    Aula06 e correções

---
+++ Aula05/Ex03_NumMulti3/Program.cs
@@ -0,0 +1,10 @@
+﻿int soma = 0;
+
+for (int i = 51; i <= 500; i+= 3)
+{
+    if (i % 2 == 1)
+    {
+       soma+=i;
+    }
+}
+Console.WriteLine($"A soma de todos os números ímpares múltiplos de 3 entre 50 e 500 é: {soma}");
